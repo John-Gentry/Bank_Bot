edeb0acecec60f556c721e531ad975e034679196
JohnJGentryII <48639136+JohnJGentryII@users.noreply.github.com>
2019-03-16 21:52:22 -0800

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -16,6 +16,12 @@ from discord.ext import commands
 import gspread
 authorized_users = ["229086359833280512","226439688330674186","426468000934264853"]
 TOKEN = 'NTU2Njk4NjYzODU5MTI2Mjcy.D29hlw.iwZqFlvGM2mheZYoMkg2L4DklCQ'
+logging.basicConfig(level=logging.WARNING)
+description = '''Bot description here2'''
+client = commands.Bot(command_prefix='>', description=description)
+@client.event
+async def on_ready():
+    print('Bot is ready for use')
 
 async def list_servers():
     await client.wait_until_ready()
@@ -24,3 +30,6 @@ async def list_servers():
         for server in client.servers:
             print(server.name)
         await asyncio.sleep(600)
+if __name__ == '__main__':
+    client.loop.create_task(list_servers())
+    client.run(TOKEN)
