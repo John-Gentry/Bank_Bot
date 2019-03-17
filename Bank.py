a92ae31b82b992c0ddcfd383b682d98b29216e17
JohnJGentryII <48639136+JohnJGentryII@users.noreply.github.com>
2019-03-16 21:26:36 -0800

    Create Bank.py

---
+++ Bank.py
@@ -0,0 +1,26 @@
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
+import json, requests
+import time
+import re
+import os
+import sys
+import discord
+import logging
+from discord.ext import commands
+#import mechanicalsoup
+import gspread
+authorized_users = ["229086359833280512","226439688330674186","426468000934264853"]
+TOKEN = 'NTU2Njk4NjYzODU5MTI2Mjcy.D29hlw.iwZqFlvGM2mheZYoMkg2L4DklCQ'
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
